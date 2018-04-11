9fe529a6fcc2f6c7fe808b99400a19096e5acf57
Raphael Lienard <raphael.lienard@i-ctg.com>
2018-04-11 08:10:31 +0200

    test

---
+++ devnet/apicem-apis-with-python-sample-codes/basic-labs/apicem_config.py
@@ -0,0 +1,12 @@
+# Step 1
+# Change apic-em IP to the one you are using
+# apicem_ip =  "sandboxapicem.cisco.com"
+# apicem_ip is a string
+APICEM_IP = ""
+
+# Step 2
+# Eneter user name and password to get a service ticket
+# If you assign username, password and version here you don't need to pass parameter when calling
+USERNAME = ""
+PASSWORD = ""
+VERSION = "v1"
