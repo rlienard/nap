8f6ca88b81afe3bd91baf70fdc63c9ca9176b1db
Raphael Lienard <raphael.lienard@i-ctg.com>
2018-04-12 15:21:13 +0200

    Update apicem_config.py

--- devnet/apicem-apis-with-python-sample-codes/basic-labs/apicem_config.py
+++ devnet/apicem-apis-with-python-sample-codes/basic-labs/apicem_config.py
@@ -2,11 +2,11 @@
 # Change apic-em IP to the one you are using
 # apicem_ip =  "sandboxapicem.cisco.com"
 # apicem_ip is a string
-APICEM_IP = "10.138.50.95"
+APICEM_IP = ""
 
 # Step 2
 # Eneter user name and password to get a service ticket
 # If you assign username, password and version here you don't need to pass parameter when calling
-USERNAME = "admin"
-PASSWORD = "!Bluefish"
+USERNAME = ""
+PASSWORD = ""
 VERSION = "v1"
