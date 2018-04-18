a6fedc579b9ad55330d7b935da2d0fc1b53f6858
Raphael Lienard <raphael.lienard@i-ctg.com>
2018-04-18 16:48:16 +0200

    lots of stuff

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -25,3 +25,23 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+class NewdeviceForm(FlaskForm):
+    members_in_stack = StringField('Number of stack members')
+    uplinks = StringField('Number of uplinks')
+    vlan_interco_vdl = StringField('VDL interco VLANID')
+    second_byte_mgmt_vdl = StringField('Second byte Mgmt IP')
+    third_byte_mgmt_vdl = StringField('Third byte Mgmt IP')
+    fourth_byte_mgmt_vdl = StringField('Fourth byte Mgmt IP')
+    second_byte_interco_tl = StringField('Second byte IP Interco TL')
+    third_byte_interco_tl = StringField('Third byte IP Interco TL')
+    third_byte_hc = StringField('Third byte IP Hotcity')
+    hostname = StringField('Hostname')
+    accessport_nb = StringField('Number of switch ports')
+    snmp_location = StringField('SNMP Location')
+    submit = SubmitField('Create')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, NewdeviceForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -21,15 +28,41 @@ def index():
     ]
     return render_template("index.html", title='Home Page', posts=posts)
 
-@app.route('/operations')
+@app.route('/operations', methods=['GET', 'POST'])
 @login_required
 def operations():
-    return render_template('operations.html', title='Operations')
+    form = NewdeviceForm()
+    return render_template('operations.html', title='Operations', form=form)
 
-@app.route('/settings')
+@app.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
     return render_template('settings.html', title='Settings')
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,16 +80,6 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/logout')
 def logout():
