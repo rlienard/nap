05c4309bc13ae4b665e9220424b91edbb20eed04
Raphael Lienard <raphael.lienard@i-ctg.com>
2018-04-23 14:33:47 +0200

    jQuery added

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -2,11 +2,46 @@ $(function()
 {
   $("#div_l3").hide();
 
-  if(document.getElementById('is_l3').checked)
+  $('#is_l3').change(function()
   {
-    $("#div_l3").show();
-    $("#div_l3_tl").hide();
-    $("#div_l3_hc").hide();
-    $("#div_l2").hide();
-  }
+    if($(this).prop("checked"))
+    {
+      $('#div_l2').hide("slow");
+      $('#div_l3').show("slow");
+      $('#div_l3_hc').hide("slow");
+      $('#div_l3_tl').hide("slow");
+    }
+
+    else
+    {
+      $('#div_l2').show("slow");
+      $('#div_l3').hide("slow");
+    }
+  });
+
+  $('#has_tl').change(function()
+  {
+    if($(this).prop("checked") && $('#is_l3').prop("checked"))
+    {
+      $('#div_l3_tl').show("slow");
+    }
+
+    else
+    {
+      $('#div_l3_tl').hide("slow");
+    }
+  });
+
+  $('#has_hc').change(function()
+  {
+    if($(this).prop("checked") && $('#is_l3').prop("checked"))
+    {
+      $('#div_l3_hc').show("slow");
+    }
+
+    else
+    {
+      $('#div_l3_hc').hide("slow");
+    }
+  });
 });
